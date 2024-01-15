a36f5af15397ffba31c5e5f2a4282b12e98b47f1
PETERGS27 <p.gusev777@protonmail.com>
2024-01-15 22:06:45 +0300

    snapshot1

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,7 @@
-print('a')
+import telebot
+import config
+bot = telebot.TeleBot(config.TOKEN)
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    bot.send_message(message.chat.id, message.text)
+bot.polling(none_stop=True)
