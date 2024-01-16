2c4e77683ce27220f10bcb5bea4d0db07b065207
PETERGS27 <p.gusev777@protonmail.com>
2024-01-16 21:37:44 +0300

    fix1

--- Main/main.py
+++ Main/main.py
@@ -3,16 +3,16 @@ import config
 bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Здрвствуй, {message.from_user.first_name} этот бот создан для облегчения жизни ЗОЖников.\n\
+    bot.send_message(message.chat.id, f'Здарвствуй, {message.from_user.first_name} этот бот создан для облегчения жизни ЗОЖников.\n\
 Вот список функций:\n\
 /start - Запуск бота\n\
 /help - Показать все команды\n\
-/getbzu - Расчитать БЖУ\n\
+/getbzu - Рассчитать БЖУ\n\
 Для начала работы выберите необходимую функцию.')
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, f'/start - Запуск бота\n\
 /help - Показать все команды\n\
-/getbzu - Расчитать БЖУ')
+/getbzu - Рассчитать БЖУ')
 
 bot.infinity_polling()
