9cf4faf51d7502a312a7d5f9eb7570c6c1e6ec3c
PETERGS27 <p.gusev777@protonmail.com>
2024-01-17 19:26:24 +0300

    snapshot 17.01.2024 19:26

--- Main/main.py
+++ Main/main.py
@@ -1,14 +1,22 @@
 import telebot
 import config
+from telebot import types
+
 bot = telebot.TeleBot(config.TOKEN)
+
 @bot.message_handler(commands=['start'])
 def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    bzubtn = types.KeyboardButton('Рассчитать БЖУ')
+    helpbtn = types.KeyboardButton('Показать все команды')
+    markup.row(bzubtn, helpbtn)
     bot.send_message(message.chat.id, f'Здравствуй, {message.from_user.first_name} этот бот создан для облегчения жизни ЗОЖников.\n\
 Вот список функций:\n\
 /start - Запуск бота\n\
 /help - Показать все команды\n\
 /getbzu - Рассчитать БЖУ\n\
-Для начала работы выберите необходимую функцию.')
+Для начала работы выберите необходимую функцию.', reply_markup=markup)
+
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, f'/start - Запуск бота\n\
